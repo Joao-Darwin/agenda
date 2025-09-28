d34b75b06eba364807ba9fa71f66dddc031cc5f9
João Darwin <joaodarwin.ip22@gmail.com>
2025-09-28 17:30:59 -0300

    feat: Configurando registro de Contact no admin

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,8 @@ from contact.models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',) # Colunas que serão mostradas na tabela no /admin
+    ordering = ('id',) # Coluna de ordenação padrão
+    list_filter = ('created_at',) # Filtros disponíveis
+    search_fields = ('id', 'first_name', 'last_name',) # Colunas de pesquisas disponíveis
+    list_per_page = 10 # Quant de 'linhas' por página na tabela
