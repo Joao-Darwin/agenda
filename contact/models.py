7a656a2e558d70b626230355fdd9d6d62e090a74
João Darwin <joaodarwin.ip22@gmail.com>
2025-10-11 12:32:56 -0300

    feat: Criar relacionamento de contato com owner

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -24,6 +25,14 @@ class Contact(models.Model):
 
     category = models.ForeignKey(
         Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="contacts"
+    )
+
+    owner = models.ForeignKey(
+        User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
