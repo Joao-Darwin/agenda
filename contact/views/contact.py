bf24c34cd756ec9e58dc1885b329668321097c8a
João Darwin <joaodarwin.ip22@gmail.com>
2025-10-25 20:01:15 -0300

    feat: Criando template de contato específico

--- contact/views/contact.py
+++ contact/views/contact.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 
@@ -8,3 +8,11 @@ def index(request):
     context = {'contacts': contacts}
 
     return render(request, 'contact/index.html', context)
+
+
+def contact(request, id):
+    single_contact = get_object_or_404(Contact, pk=id, show=True)
+
+    context = {'contact': single_contact}
+
+    return render(request, 'contact/contact.html', context)
